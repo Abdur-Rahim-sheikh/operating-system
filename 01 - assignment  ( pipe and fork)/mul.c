1f7b63f4782806c08644904999a6a630743c5005
Abdur  Rahim <39773406+Abdur-Rahim-sheikh@users.noreply.github.com>
2021-08-15 11:47:11 +0600

    Add files via upload

---
+++ 01 - assignment  ( pipe and fork)/mul.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+
+char *itoa(int n){
+    char *sum;
+    sum = malloc(20*sizeof(char));
+    int flag = (n<0);
+    n = abs(n);
+    int i=0;
+    if(n==0){
+        return "0";
+    }
+    while(n){
+        sum[i]=n%10+'0';
+        n/=10;
+        i++;
+    }
+    if(flag){
+        sum[i]='-';
+        i++;
+    }
+    sum[i]='\0';
+    for(int k=0;2*k<i;k++){
+        char ch = sum[k];
+        sum[k] = sum[i-k-1];
+        sum[i-k-1] = ch;
+    }
+    
+    return (char *)sum;
+}
+
+
+int main(int argn,char *argv[]){
+    // for(int i=0;i<argn;i++){
+    //     printf("%s\n",argv[i]);
+    // }
+    int fd0 = atoi(argv[0]);
+    int fd1 = atoi(argv[1]);
+    int a = atoi(argv[2]);
+    int b = atoi(argv[3]);
+    close(fd0);
+    char *mul = itoa(a*b);
+    // printf("multiplied: %s\n",mul);
+    write(fd1,mul,strlen(mul)+1);
+    
+    return 0;
+}
